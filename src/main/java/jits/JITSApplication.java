dc79d80015e9d20d3f80ccae2b5fa7eb06de19c0
Ruben Hakobyan <hakobyanruben2000@gmail.com>
2021-02-06 01:30:52 +0000

    Initial Commit
    Setup files and configurations for the project.
    Include neccessary libraries.

---
+++ src/main/java/jits/JITSApplication.java
@@ -0,0 +1,13 @@
+package jits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JITSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JITSApplication.class, args);
+	}
+
+}
